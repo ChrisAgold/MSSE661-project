47db1776339535baf5864069fff727e6b7be4c32
Chris Agold <christophersagold@gmail.com>
2022-03-15 12:13:37 -0500

    added styles and js

---
+++ CLASS-APP/week-2/src/app.js
@@ -0,0 +1,41 @@
+var array = [1, 2, 3, 4, 5].sort(function (a, b) {
+});
+
+function sort(a, b) {
+    if (a > b) {
+        return 1; // true
+    } else if (a < b) {
+        return -1; // false
+    }
+    return 0; // false
+}
+
+// OBJECTS
+var object = {
+    name: 'Mittens',
+    age: 50
+};
+
+class Person {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getLegalAge() {
+}
+}
+
+console.log(new Person('Mittens', 50))
+
+function getLegalAge(Person) {
+    if (this.age > 21) {
+        return 1;
+    }
+    return -1;
+}
+
+console.log(getLegalAge(new Person("Cat", 25)))
