592315e88bc7224f7690df2601d558e27a9d1dcf
Chris Agold <christophersagold@gmail.com>
2022-03-18 13:00:13 -0500

    added css & js to server

--- CLASS-APP/week-3/server/index.js
+++ CLASS-APP/week-3/server/index.js
@@ -1,14 +1,20 @@
+// Initialize express for server
 const express = require('express');
 const app = express();
 
 // SERVE PUBLIC DIRECTORY
 app.use(express.static('public'));
 
+// GET CSS & JS FILES TO SERVER
+app.use('/css', express.static(__dirname + '/public/css')); // http://localhost:3000/css
+app.use('/js', express.static(__dirname + '/public/src'));
+
+// SERVER START AT PORT 3000
 app.listen(3000, function() {
     console.log("Server started at http://localhost:%s", 3000);
 });
 
-
+// RUN node server/index.js IN TERMINAL
 
 
 
