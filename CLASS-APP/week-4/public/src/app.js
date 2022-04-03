da274dfd7b3c1917fdc3f81d32863c1262ab00de
Chris Agold <christophersagold@gmail.com>
2022-04-03 08:35:28 -0500

    added files week 4

---
+++ CLASS-APP/week-4/public/src/app.js
@@ -0,0 +1,26 @@
+// OBJECTS
+var object = {
+    name: 'Mittens',
+    age: 50
+};
+
+class Person {
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getLegalAge() {
+        if (this.age > 21) {
+            return 1;
+        }
+        return -1;
+    }
+}
+
+const person = new Person('Mittens', 2);
+// console.log(person.getLegalAge());
+
